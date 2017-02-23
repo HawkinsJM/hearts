2435db88316a33d7966b110e4d591aa6d38830d7
Jeffrey Hawkins <jeffreyhawkins@Jeffreys-MacBook-Pro.local>
2017-02-23 13:34:31 -0800

    reuploading mp3

--- heart_sketch.js
+++ heart_sketch.js
@@ -1,14 +1,12 @@
 //make it fit the window and acomidate resiing - couldnt get live resizing to work...
 
-
-
 var table;
 var text_color = "#FADADD"
 var bg_color = "#FEB9C7"
+var heart_fill = '#C43B53'
+var heart_stroke = '#DD6C89'
+
 function draw_heart(x,y,s) {
-  stroke('#DD6C89');
-  fill('#C43B53');
-  strokeWeight(2);
   var h_scale = s
   var adj = h_scale/(2*sqrt(2));
 
@@ -23,7 +21,6 @@ function draw_heart(x,y,s) {
        rect(0, 0, h_scale, h_scale);
        pop();
 
-     noFill();
      beginShape();
      vertex(0, h_scale);
      vertex(h_scale, h_scale);
@@ -34,9 +31,11 @@ function draw_heart(x,y,s) {
 }
 
 function Heart(start, x, y) {
-  this.speed = 7*random([1,1.5,2,2.5,3]);
-  wh = (windowHeight/10)*1.2
+  this.speed = 7*random([1,1.25,1.5,1.75, 2.25, 2.5]);
+
+  wh = (min(windowHeight,windowWidth)/10)*1.2
   this.h_scale = random(wh*0.5, wh*1);
+
   if (x == "None"){
   this.x = random(0+this.h_scale/2,width-this.h_scale);
   this.y = random(this.h_scale/2+height, this.h_scale/2+height*1.5);
@@ -66,13 +65,15 @@ function Heart(start, x, y) {
 function Message(m_text, start) {
   this.m_text= m_text
   this.m_text_length = this.m_text.length;
-  this.m_text_size = random(windowHeight/15,windowHeight/10);
+  this.text_scale = min(windowHeight,windowWidth)
+  this.m_text_size = random(this.text_scale/15,this.text_scale/10);
   this.width = random(windowWidth*.3334,windowWidth);
   this.height = this.m_text_size*4
   this.h_scale = this.m_text_length*this.m_text_size/200
 
   this.x = random(0,width-this.width);
-  this.speed = 7*random([1,1.5,2,2.5,3]);
+  this.timeonscreen = random([.3,.4,.5,.6,.7]);
+  this.speed = windowHeight/(60*this.timeonscreen);
   this.y = random(this.h_scale/2+height, this.h_scale/2+height*1.5);
   this.start = start;
 
@@ -111,37 +112,9 @@ function preload() {
 var hearts = []; // array of heart objects
 var messages = []; // array of message objects
 
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-//
-//   //blanks the arrays and then refils them based on new window dimensions
-//   var hearts = []; // array of heart objects
-//   var messages = []; // array of message objects
-//
-//   print(hearts.length)
-//
-//   for (var i = 0; i < table.getRowCount(); i++) {
-//     var m_text = table.getString(i,'text');
-//     if (m_text != ''){
-//       //print(m_text)
-//       messages.push(new Message(m_text, start_time));
-//     }
-//
-//     var heart_count = int(table.getString(i,'hearts'));
-//     var start_time = int(table.getString(i,'duration'));
-//
-//     for (var j = 0; j < heart_count; j++) {
-//       hearts.push(new Heart(start_time, "None", "None"));
-//     }
-//
-//   }
-// print(hearts[2])
-// print(hearts.length)
-// }
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(120);
+  //frameRate(120);
   noCursor();
 
   for (var i = 0; i < table.getRowCount(); i++) {
@@ -177,25 +150,39 @@ function draw() {
   background(bg_color);
   var play_time = she.currentTime();
   var time = str(floor(play_time));
+
+  //draw and move all the hearts
   var t0 = performance.now();
+  stroke(heart_stroke);
+  fill(heart_fill);
+  strokeWeight(2);
   for (var i = 0; i < hearts.length; i++) {
     hearts[i].display(time)
   }
   var t1 = performance.now();
 
+  //draw and move all the messages
   var t0 = performance.now();
   for (var i = 0; i < messages.length; i++) {
     messages[i].display(time)
   }
   var t1 = performance.now();
+
+//draws a small hearrt under the mouse cursor
+  stroke(heart_stroke);
+  fill(heart_fill);
+  strokeWeight(2);
   draw_heart(mouseX, mouseY, 10);
-  if (time>365) {
+
+  //jumps to ending page
+  if (time>353) {
     window.location.href = 'end.html';
   }
 
 }
 
 function mouseClicked() {
+  //adds a heart to the hearts array on mouse click
   var play_time = she.currentTime();
   var time = str(floor(play_time));
   hearts.push(new Heart(0, mouseX, mouseY));
